e14b526740c9fdbc86ed735ab27e90988b9384e5
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-12-20 16:14:35 +0100

    v1.1

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BadWords</title>
 </head>
+
 <body>
     <p>
-    <?php 
-    $paragraph="Ciao utente, come va?"; 
-    echo($paragraph);
-    echo(strlen($paragraph));
-    $badWord=$_GET["badWord"];
-    $paragraph=str_replace($badWord,"***",$paragraph);
-    echo($paragraph);
-    echo(strlen($paragraph));
-    ?>
+        <?php
+        $paragraph = "Ciao utente, come va?";
+        echo ($paragraph);
+        echo (strlen($paragraph));
+        $badWord = $_GET["badWord"];
+        $paragraph = str_replace($badWord, "***", $paragraph);
+        echo ($paragraph);
+        echo (strlen($paragraph));
+        ?>
     </p>
 </body>
+
 </html>
